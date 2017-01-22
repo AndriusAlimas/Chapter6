1c35c2003def91c6f233acd912f7b627b39e94dc
Andrius <andriusjavait@gmail.com>
2017-01-22 18:50:24 +0000

    added Servlet and DD map it

---
+++ Chapter6/com.example.servlet/com/example/servlet/MyCookieServlet.java
@@ -0,0 +1,15 @@
+package com.example.servlet;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+
+public class MyCookieServlet extends HttpServlet {
+	protected void doGet(HttpServletRequest request, 
+			HttpServletResponse response)throws ServletException,IOException{
+		
+	}
+	protected void doPost(HttpServletRequest request, 
+			HttpServletResponse response)throws ServletException,IOException{
+		
+	}
+}
