0b593fc685b1082ea82f219bd9c4a2949f5695b4
Andrius <andriusjavait@gmail.com>
2017-01-26 12:23:37 +0000

    update MyCookieServlet code doGet method to show how to print all cookies and set status

--- Chapter6/src/com/example/servlet/MyCookieServlet.java
+++ Chapter6/src/com/example/servlet/MyCookieServlet.java
@@ -6,7 +6,35 @@ import java.io.*;
 public class MyCookieServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, 
 			HttpServletResponse response)throws ServletException,IOException{
-		
+		// Sets the status code for this response. This method is used to set the return status code when there
+		// is no error (for example, for the status codes SC_OK or SC_MOVED_TEMPORARILY). If there is an error,
+		// and the caller wishes to invoke an error-page defined in the web application, the sendError method 
+		// should be used instead:
+		response.setStatus(404);
+        Cookie cookie = null;
+        Cookie[] arrcookie = null;
+        // using HttpServletRequest we get all cookies:
+        arrcookie = request.getCookies();
+        
+        response.setContentType("text/html");
+        PrintWriter pw = response.getWriter();
+        String title = "Reading Cookies Example";
+        String docType = "<!doctype html public \"-//w3c//dtd html 4.0 transitional//en\">\n";
+        String color = "#CBfCf0";
+        pw.println(docType + "<html>\n<head><title>" + title + "</title></head>\n<body bgcolor=\""+color+"\">\n");
+        // this code simple retrieve all cookie names and values:
+        if (arrcookie != null) {
+            pw.println("<h2> Found Cookies Name and Value</h2>");
+            for (int i = 0; i < arrcookie.length; ++i) {
+                cookie = arrcookie[i];
+                pw.print("Name : " + cookie.getName() + ",  ");
+                pw.print("Value: " + cookie.getValue() + " <br/>");
+            }
+        } else {
+            pw.println("<h2>No cookies founds</h2>");
+        }
+        pw.println("</body>");
+        pw.println("</html>");
 	}
 	protected void doPost(HttpServletRequest request, 
 			HttpServletResponse response)throws ServletException,IOException{
